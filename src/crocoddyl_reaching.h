e5d88f140bfe3167589001230fbda3f439354851
mfourmy <mederic.fourmy@gmail.com>
2023-03-13 16:41:59 +0100

    remove goal ref from config and implement a set reference function in C++ class

--- src/crocoddyl_reaching.h
+++ src/crocoddyl_reaching.h
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-
 #include <pinocchio/fwd.hpp>
 #include <boost/algorithm/string.hpp>
 #include <algorithm>
@@ -12,8 +11,6 @@
 #include <pinocchio/multibody/model.hpp>
 #include <pinocchio/multibody/frame.hpp>
 
-
-
 #include <crocoddyl/core/optctrl/shooting.hpp>
 #include <crocoddyl/core/solver-base.hpp>
 #include <crocoddyl/core/utils/exception.hpp>
@@ -23,7 +20,6 @@
 #include <crocoddyl/core/activations/weighted-quadratic.hpp>
 // #include <crocoddyl/core/activations/weighted-quadratic-barrier.hpp>
 // #include <crocoddyl/core/activations/quadratic-flat-log.hpp>
-
 
 #include <crocoddyl/core/fwd.hpp>
 #include <crocoddyl/core/solvers/ddp.hpp>
@@ -43,129 +39,120 @@
 #include <crocoddyl/multibody/residuals/frame-velocity.hpp>
 // #include <crocoddyl/multibody/residuals/contact-force.hpp>
 
-
-
 namespace pin = pinocchio;
-
 
 struct CrocoddylConfig
 {
-    unsigned int T;   // nb of nodes - terminal one
+    unsigned int T; // nb of nodes - terminal one
     double dt_ocp;
 
-    Eigen::Matrix<double,14,1> x0;
+    Eigen::Matrix<double, 14, 1> x0;
     std::string ee_frame_name;
-    Eigen::Vector3d goal_trans;
 
-    Eigen::Matrix<double,7,1> armature;
-
-
-
+    Eigen::Matrix<double, 7, 1> armature;
 };
-
-
 
 class CrocoddylReaching
 {
-    public:
-
+public:
     CrocoddylReaching(pin::Model _model_pin, CrocoddylConfig _config)
     {
-
+        config_ = _config;
         auto end_effector_frame_id = _model_pin.getFrameId(_config.ee_frame_name);
 
         std::cout << "Creating state, actuation and IAMs... " << std::endl;
         boost::shared_ptr<crocoddyl::StateMultibody> state = boost::make_shared<crocoddyl::StateMultibody>(boost::make_shared<pinocchio::Model>(_model_pin));
-        boost::shared_ptr<crocoddyl::ActuationModelFull> actuation =  boost::make_shared<crocoddyl::ActuationModelFull>(state);
-
+        boost::shared_ptr<crocoddyl::ActuationModelFull> actuation = boost::make_shared<crocoddyl::ActuationModelFull>(state);
 
         /*****
-         * Task weigths 
-        */
+         * Task weigths
+         */
         double w_running_frame_low = 0.0;
         double w_running_frame_high = 0.0;
         double w_frame_terminal = 1000.0;
 
         double w_frame_vel_terminal = 10.0;
-        
+
         double w_x_reg_running = 1.0;
-        Eigen::Matrix<double,7,1> diag_q_reg_running = 0*Eigen::Matrix<double,7,1>::Ones();  
-        Eigen::Matrix<double,7,1> diag_v_reg_running = Eigen::Matrix<double,7,1>::Ones();  
-        Eigen::Matrix<double,14,1> diag_x_reg_running; diag_x_reg_running << diag_q_reg_running, diag_v_reg_running;
-        
+        Eigen::Matrix<double, 7, 1> diag_q_reg_running = 0 * Eigen::Matrix<double, 7, 1>::Ones();
+        Eigen::Matrix<double, 7, 1> diag_v_reg_running = Eigen::Matrix<double, 7, 1>::Ones();
+        Eigen::Matrix<double, 14, 1> diag_x_reg_running;
+        diag_x_reg_running << diag_q_reg_running, diag_v_reg_running;
+
         double w_x_reg_terminal = 10.0;
-        Eigen::Matrix<double,7,1> diag_q_reg_terminal = 0*Eigen::Matrix<double,7,1>::Ones();  
-        Eigen::Matrix<double,7,1> diag_v_reg_terminal = Eigen::Matrix<double,7,1>::Ones();  
-        Eigen::Matrix<double,14,1> diag_x_reg_terminal; diag_x_reg_terminal << diag_q_reg_terminal, diag_v_reg_terminal;
-        
+        Eigen::Matrix<double, 7, 1> diag_q_reg_terminal = 0 * Eigen::Matrix<double, 7, 1>::Ones();
+        Eigen::Matrix<double, 7, 1> diag_v_reg_terminal = Eigen::Matrix<double, 7, 1>::Ones();
+        Eigen::Matrix<double, 14, 1> diag_x_reg_terminal;
+        diag_x_reg_terminal << diag_q_reg_terminal, diag_v_reg_terminal;
+
         double w_u_reg_running = 0.01;
-        Eigen::Matrix<double,7,1> diag_u_reg_running; diag_u_reg_running << 1, 1, 1, 1, 1, 1, 10;
+        Eigen::Matrix<double, 7, 1> diag_u_reg_running;
+        diag_u_reg_running << 1, 1, 1, 1, 1, 1, 10;
+
+        // !! translation reference should be set with set_goal_translation
+        Eigen::Vector3d dummy_translation_reference;
 
         // Frame translation
-        boost::shared_ptr<crocoddyl::CostModelAbstract> frame_goal_cost = 
-            boost::make_shared<crocoddyl::CostModelResidual>(state, 
-                boost::make_shared<crocoddyl::ResidualModelFrameTranslation>(state, end_effector_frame_id, _config.goal_trans, actuation->get_nu()));
+        boost::shared_ptr<crocoddyl::CostModelAbstract> frame_goal_cost =
+            boost::make_shared<crocoddyl::CostModelResidual>(state,
+                                                             boost::make_shared<crocoddyl::ResidualModelFrameTranslation>(state, end_effector_frame_id, dummy_translation_reference, actuation->get_nu()));
 
         // Frame velocity cost
-        boost::shared_ptr<crocoddyl::CostModelAbstract> frame_velocity_cost = 
-            boost::make_shared<crocoddyl::CostModelResidual>(state,  
-                boost::make_shared<crocoddyl::ResidualModelFrameVelocity>(state, end_effector_frame_id, pinocchio::Motion::Zero(), pinocchio::LOCAL_WORLD_ALIGNED, actuation->get_nu()));
-        
+        boost::shared_ptr<crocoddyl::CostModelAbstract> frame_velocity_cost =
+            boost::make_shared<crocoddyl::CostModelResidual>(state,
+                                                             boost::make_shared<crocoddyl::ResidualModelFrameVelocity>(state, end_effector_frame_id, pinocchio::Motion::Zero(), pinocchio::LOCAL_WORLD_ALIGNED, actuation->get_nu()));
 
-        running_models_ = std::vector< boost::shared_ptr<crocoddyl::ActionModelAbstract> >(_config.T);
+        running_models_ = std::vector<boost::shared_ptr<crocoddyl::ActionModelAbstract>>(_config.T);
 
-        std::string goal_cost_name = "translation";
+        goal_cost_name_ = "translation_cost";
 
-        for (int i=0; i < _config.T; i++)
+        for (int i = 0; i < _config.T; i++)
         {
-
             // State reg
-            boost::shared_ptr<crocoddyl::CostModelAbstract> state_reg_cost = 
+            boost::shared_ptr<crocoddyl::CostModelAbstract> state_reg_cost =
                 boost::make_shared<crocoddyl::CostModelResidual>(state,
-                                                        boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(diag_x_reg_running),
-                                                        boost::make_shared<crocoddyl::ResidualModelState>(state, _config.x0, actuation->get_nu()));
+                                                                 boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(diag_x_reg_running),
+                                                                 boost::make_shared<crocoddyl::ResidualModelState>(state, _config.x0, actuation->get_nu()));
 
-            // Ctrl reg 
+            // Ctrl reg
             boost::shared_ptr<crocoddyl::CostModelAbstract> ctrl_reg_cost =
                 boost::make_shared<crocoddyl::CostModelResidual>(state,
-                                                        boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(diag_u_reg_running),
-                                                        boost::make_shared<crocoddyl::ResidualModelControlGrav>(state, actuation->get_nu()));
+                                                                 boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(diag_u_reg_running),
+                                                                 boost::make_shared<crocoddyl::ResidualModelControlGrav>(state, actuation->get_nu()));
 
-
-
-            boost::shared_ptr<crocoddyl::CostModelSum> runningCostModel = boost::make_shared<crocoddyl::CostModelSum>(state);            
+            boost::shared_ptr<crocoddyl::CostModelSum> runningCostModel = boost::make_shared<crocoddyl::CostModelSum>(state);
             runningCostModel.get()->addCost("state_reg", state_reg_cost, w_x_reg_running);
             runningCostModel.get()->addCost("ctrl_reg", ctrl_reg_cost, w_u_reg_running);
-            runningCostModel.get()->addCost(goal_cost_name, frame_goal_cost, 0.0);  // TODO: weight schedule
-
+            runningCostModel.get()->addCost(goal_cost_name_, frame_goal_cost, 0.0); // TODO: weight schedule
 
             boost::shared_ptr<crocoddyl::DifferentialActionModelFreeFwdDynamics> runningDAM =
                 boost::make_shared<crocoddyl::DifferentialActionModelFreeFwdDynamics>(state, actuation, runningCostModel);
             runningDAM->set_armature(_config.armature);
+            // Deactivate goal cost by default until a proper reference is set
+            runningDAM->get_costs()->changeCostStatus(goal_cost_name_, false);
 
             running_models_[i] = boost::make_shared<crocoddyl::IntegratedActionModelEuler>(runningDAM, _config.dt_ocp);
         }
 
-
-        boost::shared_ptr<crocoddyl::CostModelSum> terminalCostModel = boost::make_shared<crocoddyl::CostModelSum>(state); 
+        boost::shared_ptr<crocoddyl::CostModelSum> terminalCostModel = boost::make_shared<crocoddyl::CostModelSum>(state);
         // State reg
-        boost::shared_ptr<crocoddyl::CostModelAbstract> state_reg_cost = 
+        boost::shared_ptr<crocoddyl::CostModelAbstract> state_reg_cost =
             boost::make_shared<crocoddyl::CostModelResidual>(state,
-                                                    boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(diag_x_reg_terminal),
-                                                    boost::make_shared<crocoddyl::ResidualModelState>(state, _config.x0, actuation->get_nu()));
+                                                             boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(diag_x_reg_terminal),
+                                                             boost::make_shared<crocoddyl::ResidualModelState>(state, _config.x0, actuation->get_nu()));
 
         terminalCostModel.get()->addCost("state_reg", state_reg_cost, w_x_reg_terminal);
-        terminalCostModel.get()->addCost(goal_cost_name, frame_goal_cost, w_frame_terminal);
+        terminalCostModel.get()->addCost(goal_cost_name_, frame_goal_cost, w_frame_terminal);
         terminalCostModel.get()->addCost("terminal_vel", frame_velocity_cost, w_frame_vel_terminal);
-
 
         boost::shared_ptr<crocoddyl::DifferentialActionModelFreeFwdDynamics> terminalDAM =
             boost::make_shared<crocoddyl::DifferentialActionModelFreeFwdDynamics>(state, actuation, terminalCostModel);
         terminalDAM->set_armature(_config.armature);
+        terminalDAM->get_costs()->changeCostStatus(goal_cost_name_, false);
 
         boost::shared_ptr<crocoddyl::ActionModelAbstract> terminal_model = boost::make_shared<crocoddyl::IntegratedActionModelEuler>(terminalDAM, 0.0);
 
-        // Shooting problem             
+        // Shooting problem
         boost::shared_ptr<crocoddyl::ShootingProblem> shooting_problem =
             boost::make_shared<crocoddyl::ShootingProblem>(_config.x0, running_models_, terminal_model);
         ddp_ = boost::make_shared<crocoddyl::SolverFDDP>(shooting_problem);
@@ -175,18 +162,45 @@ class CrocoddylReaching
         callbacks.push_back(boost::make_shared<crocoddyl::CallbackVerbose>());
 
         std::cout << "ddp problem set up " << std::endl;
-
     }
 
+    void set_goal_translation(Eigen::Vector3d trans)
+    {
+        // Running
+        for (size_t node_index = 0; node_index < config_.T; node_index++)
+        {
+            auto runningIAM = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(ddp_->get_problem()->get_runningModels()[node_index]);
+            auto runningDAM = boost::static_pointer_cast<crocoddyl::DifferentialActionModelFreeFwdDynamics>(runningIAM->get_differential());
+            auto frame_res_running = boost::static_pointer_cast<crocoddyl::ResidualModelFrameTranslation>(runningDAM->get_costs()->get_costs().at(goal_cost_name_)->cost->get_residual());
+            frame_res_running->set_reference(trans);
+            if (!goal_translation_set_)
+            {
+                runningDAM->get_costs()->changeCostStatus(goal_cost_name_, true);
+            }
+        }
 
-    void set_goal_translation(Eigen::Vector3d trans){
+        // Terminal
+        auto terminalIAM = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(ddp_->get_problem()->get_terminalModel());
+        auto terminalDAM = boost::static_pointer_cast<crocoddyl::DifferentialActionModelFreeFwdDynamics>(terminalIAM->get_differential());
+        auto frame_res_terminal = boost::static_pointer_cast<crocoddyl::ResidualModelFrameTranslation>(terminalDAM->get_costs()->get_costs().at(goal_cost_name_)->cost->get_residual());
+        frame_res_terminal->set_reference(trans);
+        terminalDAM->get_costs()->changeCostStatus(goal_cost_name_, false);
+        if (!goal_translation_set_)
+        {
+            terminalDAM->get_costs()->changeCostStatus(goal_cost_name_, true);
 
+            // No need to activate again
+            goal_translation_set_ = true;
+        }
     }
-
 
     boost::shared_ptr<crocoddyl::ActionModelAbstract> terminal_model_;
-    std::vector < boost::shared_ptr<crocoddyl::ActionModelAbstract> > running_models_;
+    std::vector<boost::shared_ptr<crocoddyl::ActionModelAbstract>> running_models_;
 
     boost::shared_ptr<crocoddyl::SolverFDDP> ddp_;
+    CrocoddylConfig config_;
 
+    std::string goal_cost_name_;
+    // safe guard
+    bool goal_translation_set_;
 };
--- src/main_reaching.cpp
+++ src/main_reaching.cpp
@@ -8,46 +8,48 @@
 
 namespace pin = pinocchio;
 
-
 int main()
 {
 
     std::string ee_frame_pin = "panda_link8";
 
     pin::Model model_pin;
-    std::string model_path = "/home/mfourmy/catkin_ws/src/panda_torque_mpc/config/panda_inertias_nohand.urdf";
+    std::string model_path = "/home/mfourmy/catkin_ws/src/panda_torque_mpc/res/panda_inertias.urdf";
+    // std::string model_path = "/home/mfourmy/catkin_ws/src/panda_torque_mpc/res/panda_inertias_nohand.urdf";
     pin::urdf::buildModel(model_path, model_pin);
     pin::Data data_pin(model_pin);
 
-
     CrocoddylConfig config;
-    Eigen::Vector3d delta_trans; delta_trans << -0.3, -0.3, -0.0;  
+    Eigen::Vector3d delta_trans;
+    delta_trans << -0.3, -0.3, -0.0;
     // Number of shooting nodes (minus terminal one)
     config.T = 200;
     config.dt_ocp = 1e-2;
 
     // franka_control/config/start_pose.yaml
-    Eigen::Matrix<double,7,1> q0; q0 << 0, -0.785398163397, 0, -2.35619449019, 0, 1.57079632679, 0.785398163397;
-    Eigen::Matrix<double,7,1> v0 = Eigen::Matrix<double,7,1>::Zero();
-    Eigen::Matrix<double,14,1> x0; x0 << q0, v0;
+    Eigen::Matrix<double, 7, 1> q0;
+    q0 << 0, -0.785398163397, 0, -2.35619449019, 0, 1.57079632679, 0.785398163397;
+    Eigen::Matrix<double, 7, 1> v0 = Eigen::Matrix<double, 7, 1>::Zero();
+    Eigen::Matrix<double, 14, 1> x0;
+    x0 << q0, v0;
     config.x0 = x0;
 
     config.ee_frame_name = "panda_link8";
     pin::forwardKinematics(model_pin, data_pin, q0);
     pin::updateFramePlacements(model_pin, data_pin);
     pin::SE3 T_ee0 = data_pin.oMf[model_pin.getFrameId(config.ee_frame_name)];
-    config.goal_trans = T_ee0.translation() + delta_trans;
-
+    Eigen::Vector3d goal_trans = T_ee0.translation() + delta_trans;
 
     config.armature << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1;
 
-
     CrocoddylReaching croco_pbe(model_pin, config);
 
+    croco_pbe.set_goal_translation(goal_trans);
+
     // Warm start : initial state + gravity compensation
-    std::vector<Eigen::Matrix<double,-1,1>> xs_init; 
-    std::vector<Eigen::Matrix<double,-1,1>> us_init; 
-    for (int i=0; i < config.T; i++)
+    std::vector<Eigen::Matrix<double, -1, 1>> xs_init;
+    std::vector<Eigen::Matrix<double, -1, 1>> us_init;
+    for (int i = 0; i < config.T; i++)
     {
         xs_init.push_back(x0);
         us_init.push_back(pin::computeGeneralizedGravity(model_pin, data_pin, q0));
@@ -60,12 +62,11 @@ int main()
     croco_pbe.ddp_->solve(xs_init, us_init, 100, false);
     std::cout << "ddp problem solved, nb iterations: " << croco_pbe.ddp_->get_iter() << std::endl;
 
-    std::vector<Eigen::Matrix<double,-1,1>> xs = croco_pbe.ddp_->get_xs();
-    std::vector<Eigen::Matrix<double,-1,1>> us = croco_pbe.ddp_->get_us();
+    std::vector<Eigen::Matrix<double, -1, 1>> xs = croco_pbe.ddp_->get_xs();
+    std::vector<Eigen::Matrix<double, -1, 1>> us = croco_pbe.ddp_->get_us();
 
     croco_pbe.ddp_->solve(xs, us, 100, false);
     std::cout << "ddp problem solved with WS, nb iterations: " << croco_pbe.ddp_->get_iter() << std::endl;
-
 
     // Record results
 
@@ -78,14 +79,17 @@ int main()
     file_tau.open("tsid_out_tau.csv");
     file_T.open("tsid_out_T.csv");
 
-    file_q << "q0,q1,q2,q3,q4,q5,q6" << "\n";
-    file_v << "v0,v1,v2,v3,v4,v5,v6" << "\n";
-    file_tau << "tau0,tau1,tau2,tau3,tau4,tau5,tau6" << "\n";
-    file_T << "tx,ty,tz,ox,oy,oz,tx_r,ty_r,tz_r,ox_r,oy_r,oz_r" << "\n";
+    file_q << "q0,q1,q2,q3,q4,q5,q6"
+           << "\n";
+    file_v << "v0,v1,v2,v3,v4,v5,v6"
+           << "\n";
+    file_tau << "tau0,tau1,tau2,tau3,tau4,tau5,tau6"
+             << "\n";
+    file_T << "tx,ty,tz,ox,oy,oz,tx_r,ty_r,tz_r,ox_r,oy_r,oz_r"
+           << "\n";
 
-
-    for (int i=0; i < config.T; i++)
-    {   
+    for (int i = 0; i < config.T; i++)
+    {
 
         pin::forwardKinematics(model_pin, data_pin, xs[i].head<7>());
         pin::updateFramePlacements(model_pin, data_pin);
@@ -99,13 +103,13 @@ int main()
                << xs[i](5) << ","
                << xs[i](6) << "\n";
 
-        file_v << xs[i](7+0) << ","
-               << xs[i](7+1) << ","
-               << xs[i](7+2) << ","
-               << xs[i](7+3) << "," 
-               << xs[i](7+4) << ","
-               << xs[i](7+5) << ","
-               << xs[i](7+6) << "\n";
+        file_v << xs[i](7 + 0) << ","
+               << xs[i](7 + 1) << ","
+               << xs[i](7 + 2) << ","
+               << xs[i](7 + 3) << ","
+               << xs[i](7 + 4) << ","
+               << xs[i](7 + 5) << ","
+               << xs[i](7 + 6) << "\n";
 
         file_tau << us[i](0) << ","
                  << us[i](1) << ","
@@ -117,28 +121,24 @@ int main()
 
         // Eigen::Vector3d aa_ee = pin::log3(T_ee.rotation());
         // Eigen::Vector3d aa_r = pin::log3(x_r.rotation());
-        
+
         file_T << T_ee.translation()(0) << ","
                << T_ee.translation()(1) << ","
                << T_ee.translation()(2) << ","
                << 0.0 << ","
                << 0.0 << ","
                << 0.0 << ","
-               << config.goal_trans(0) << ","
-               << config.goal_trans(1) << ","
-               << config.goal_trans(2) << ","
+               << goal_trans(0) << ","
+               << goal_trans(1) << ","
+               << goal_trans(2) << ","
                << 0.0 << ","
                << 0.0 << ","
                << 0.0 << "\n";
     }
 
     std::cout << "Files recorded" << std::endl;
-
-
 }
-
 
 void record_results()
 {
-
 }
